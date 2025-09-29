5b56c94e1f187b21d89307b1c8ea66aae2feabf3
Rohith-122606 <eng24am0105@dsu.edu.in>
2025-09-29 12:24:45 +0530

    Update while_loop.java

--- Java/while_loop.java
+++ Java/while_loop.java
@@ -2,8 +2,12 @@ public class while_loop{
     public static void main(String[] args){
     int i = 1;
     while(i < 10){
-        System.out.println(i);
         i++;
+        int j=1;
+        while(j <= 10){
+        System.out.println(i+"*"+j+"="+i*j);
+        j++;
+            }
         }
     }
 }
