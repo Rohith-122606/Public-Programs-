b3e863f812c747e87aefa80565a22bf5e8962f8e
Rohith-122606 <eng24am0105@dsu.edu.in>
2025-09-29 12:04:52 +0530

    Add files via upload

---
+++ Java/for_loop.java
@@ -0,0 +1,14 @@
+public class for_loop{
+    public static void main(String[] a1){
+        for(int i=0; i<10; i++){
+            System.out.println(i+ "th element is:"+ i);
+        }
+
+        for(int i=0; i<=10; i++){
+            for(int j=0; j<=10; j++){
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+    }
+}
+
